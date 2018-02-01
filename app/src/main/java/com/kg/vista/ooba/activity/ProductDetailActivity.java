05bcef55114106e83ad666b0bf5119431999cedf
Elbar <Elbar_israilov@mail.ru>
2018-02-01 10:11:55 +0600

    +Added new files

--- app/src/main/java/com/kg/vista/ooba/activity/ProductDetailActivity.java
+++ app/src/main/java/com/kg/vista/ooba/activity/ProductDetailActivity.java
@@ -15,7 +15,6 @@ public class ProductDetailActivity extends AbstractActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_det
-                ail);
+        setContentView(R.layout.activity_product_detail);
     }
 }
